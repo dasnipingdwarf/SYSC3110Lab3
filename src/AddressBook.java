25d3932591ae05f667635af979b44030a12ae755
bramsrna <bramsrna@192.168.46.1>
2017-09-25 09:05:00 -0400

    Fixed the output

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,23 +1,26 @@
 import java.util.ArrayList;
 public class AddressBook {
-	private static ArrayList<BuddyInfo> buddies;
+	private ArrayList<BuddyInfo> buddies;
 	
 	public AddressBook() {
-		buddies = new ArrayList<>();
+		buddies = new ArrayList<BuddyInfo>();
 	}
 	
 	public static void main(String[] args) {
+		AddressBook book = new AddressBook();
+		
 		System.out.println("Address Book");
-		addBuddy(new BuddyInfo());
-		removeBuddy(0);
+		
+		book.addBuddy(new BuddyInfo());
+		book.removeBuddy(0);
 		
 	}
 	
-	public static void addBuddy(BuddyInfo toAdd) {
+	public void addBuddy(BuddyInfo toAdd) {
 		buddies.add(toAdd);		
 	}
 	
-	public static void removeBuddy(int index) {
+	public void removeBuddy(int index) {
 		buddies.remove(index);
 		
 	}
